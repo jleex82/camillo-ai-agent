be21723fd995603bba9952de202be1d04a923c1c
jleex82 <jleex82@gmail.com>
2026-02-25 16:04:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain.chains import LLMChain
 import yfinance as yf
 import praw
